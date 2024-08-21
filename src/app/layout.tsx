76748f9d30de317ea1a512caf06a4fd78f822290
babukre1 <108317783+babukre1@users.noreply.github.com>
2024-08-21 21:03:24 +0300

    move to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Sofia_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 const sofia = Sofia_Sans({ subsets: ["latin"] });
 
@@ -19,6 +20,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Awesome Project</title>
+        <meta property="og:title" content="Aura Interiors" />
+        <meta
+          property="og:description"
+          content="Check out this awesome project built with Next.js."
+        />
+        <meta
+          property="og:image"
+          content="https://aura-interiors.vercel.app/Link-Preview-Image.jpg"
+        />
+        <meta property="og:url" content="https://aura-interiors.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Aura Interiors" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@babubkre" />
+        <meta name="twitter:title" content="Awesome Project" />
+        <meta
+          name="twitter:description"
+          content="Check out this awesome project built with Next.js."
+        />
+        <meta
+          name="twitter:image"
+          content="https://aura-interiors.vercel.app/about-hero-image.jpg"
+        />
+      </Head>
       <body className={sofia.className}>
         <div className="flex flex-col items-center justify-center">
           <Navbar />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,40 +7,12 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Awesome Project</title>
-        <meta property="og:title" content="Aura Interiors" />
-        <meta
-          property="og:description"
-          content="Check out this awesome project built with Next.js."
-        />
-        <meta
-          property="og:image"
-          content="https://aura-interiors.vercel.app/Link-Preview-Image.jpg"
-        />
-        <meta property="og:url" content="https://aura-interiors.vercel.app" />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Awesome Project" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@babubkre" />
-        <meta name="twitter:title" content="Awesome Project" />
-        <meta
-          name="twitter:description"
-          content="Check out this awesome project built with Next.js."
-        />
-        <meta
-          name="twitter:image"
-          content="https://aura-interiors.vercel.app/about-hero-image.jpg"
-        />
-      </Head>
       <Container className="flex flex-col justify-center items-center">
         <HeroSection />
         <Portfolio />
         <Testimonials />
         <TrustedBy />
       </Container>
-    </>
   );
 }
